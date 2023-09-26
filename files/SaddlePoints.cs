55536c739f0d50f57964702c1670e402a330bc60
aRtmcX <aRtmcX@cyber-dojo.org>
2023-09-26 17:52:19 +0000

    2 ran tests, predicted red, got amber

---
+++ files/SaddlePoints.cs
@@ -0,0 +1,13 @@
+public class SaddlePoints
+{
+    private int[,] _array = new int [5,5];
+    
+    public void InitFromArray
+    {
+    }
+    
+    public List<Tuple<int,int>> getSaddlePoints()
+    {
+        return new List<Tuple<int,int>>();
+    }
+}
---
+++ files/SaddlePointsTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class SaddlePointsTest
+{
+    [Test]
+    public void Une_matrice_de_zeros_n_a_pas_de_saddle_points()
+    {
+        SaddlePoints sp = new SaddlePoints();
+        sp.InitFromArray(new Array() 
+                         { 
+                             {0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0}
+                         });
+        
+        // a simple example to start you off
+        Assert.AreEqual(0, sp.GetSaddlePoints().Count);
+    }
+}
