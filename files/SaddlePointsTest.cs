9b9c29a86c23c9e17f571b388107f3e854411830
aRtmcX <aRtmcX@cyber-dojo.org>
2023-09-26 18:13:11 +0000

    16 ran tests, predicted red, got red

--- files/SaddlePointsTest.cs
+++ files/SaddlePointsTest.cs
@@ -18,6 +18,27 @@ public class SaddlePointsTest
         Assert.AreEqual(25, sp.GetSaddlePoints().Count);
     }
     
-    
+    [Test]
+    public void Une_matrice_avec_une_colonne_incrementielle_a_un_unique_saddle_points()
+    {
+        SaddlePoints sp = new SaddlePoints();
+        sp.Zero();
+        
+        /*
+            {1, 0, 0, 0, 0},
+            {2, 0, 0, 0, 0},
+            {3, 0, 0, 0, 0},
+            {4, 0, 0, 0, 0},
+            {5, 0, 0, 0, 0}
+        */
+        sp.SetPoint(0,0,1);
+        sp.SetPoint(0,1,2);
+        sp.SetPoint(0,2,3);
+        sp.SetPoint(0,3,4);
+        sp.SetPoint(0,4,5);
+        
+        // a simple example to start you off
+        Assert.AreEqual(1, sp.GetSaddlePoints().Count);
+    }
     
 }
