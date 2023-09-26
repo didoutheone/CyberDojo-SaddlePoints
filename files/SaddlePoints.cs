47372fe7ad577e67c8e7fcff0aa9f6cba5558800
aRtmcX <aRtmcX@cyber-dojo.org>
2023-09-26 18:34:09 +0000

    17 ran tests, predicted amber, got amber

--- files/SaddlePoints.cs
+++ files/SaddlePoints.cs
@@ -7,12 +7,12 @@ public class SaddlePoints
     
     public SaddlePoints()
     {
-        Zero();
+        Zero(_array);
     }
     
-    public void Zero()
+    public void Zero(int[,] array)
     {
-        _array = new int[,]
+        array = new int[,]
         { 
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0},
@@ -28,17 +28,100 @@ public class SaddlePoints
         _array[x,y] = value;
     }
     
+    // Saddle point :
+    // value >= any in its row
+    // value <= any in its column
     public List<Tuple<int,int>> GetSaddlePoints()
     {
         List<Tuple<int,int>> result = new List<Tuple<int,int>>();
+        
+        int[,] minsEtMaxs = new int[5,5];
+        Zero(minsEtMaxs);
+        
+        // Get maxs of rows and set 1 for them in a matrix
+        for(int i = 0; i < 5; i++)
+        {
+            int[] maxs = GetPositionsOfMaximums(GetRow(i));
+            foreach(int max in maxs)
+            {
+                minsEtMaxs[i, max] = 1;
+            }
+        }
+        
+        // get mins of columns and add 1 for them in a matrix
+        for(int j = 0; j < 5; j++)
+        {
+           int[] mins = GetPositionsOfMinimums(GetColumn(j));
+            foreach(int min in mins)
+            {
+                minsEtMaxs[min, j] += 1;
+            }
+        }
+        
+        // look for values 2 in the resulting matrix
         for(int i = 0; i < 5; i++)
         {
             for(int j = 0; j < 5; j++)
             {
-                result.Add(new Tuple<int,int>(i,j));
+                if(minsEtMaxs[i,j] == 2) result.Add(new Tuple<int,int>(i,j));
             }
         }
         
         return result;
     }
+    
+    private int[] GetRow(int i)
+    {
+        return _array[i];
+    }
+    
+    private int[] GetColumn(int j)
+    {
+        return new int[] { _array[0,j], _array[1,j], _array[2,j], _array[3,j], _array[4,j] };
+    }
+                                               
+                                               
+    private int[] GetPositionsOfMaximums(int[] row)
+    {
+        int max = Int32.MinValue;
+        List<int> maximumsPositions = new List<int>();
+        for(int i = 0; i < row.Length; i++)
+        {
+            if(row[i] > max)
+            {
+                max = row[i];
+                maximumsPositions.Clear();
+                maximumsPositions.Add(i);
+            }
+            else if(row[i] == max)
+            {
+                maximumsPositions.Add(i);
+            }
+        }
+        
+        return maximumsPositions.ToArray();
+    }
+    
+    private int[] GetPositionsOfMinimums(int[] column)
+    {
+        int min = Int32.MaxValue;
+        List<int> minimumsPositions = new List<int>();
+        for(int j = 0; j < column.Length; j++)
+        {
+            if(column[j] < min)
+            {
+                min = column[j];
+                minimumsPositions.Clear();
+                minimumsPositions.Add(j);
+            }
+            else if(column[j] == min)
+            {
+                minimumsPositions.Add(j);
+            }
+        }
+        
+        return minimumsPositions.ToArray();
+    }
+    
+    
 }
