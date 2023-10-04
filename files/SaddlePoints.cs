b54270b2115cba72535834d72f5558f2d5080931
aRtmcX <aRtmcX@cyber-dojo.org>
2023-10-04 11:14:11 +0200

    Refacto pour une meilleurs lisibilité

--- files/SaddlePoints.cs
+++ files/SaddlePoints.cs
@@ -1,157 +1,160 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public class SaddlePoints
+namespace MySaddlePoints
 {
-    private int[,] _array;
-    
-    public SaddlePoints()
+    public class Cell
     {
-        _array = GetZeroArray();
-    }
-    
-    public void SetPoint(int row, int column, int value)
-    {
-        _array[row,column] = value;
-    }
-    
-    // Saddle point :
-    // value >= any in its row
-    // value <= any in its column
-    public List<Tuple<int,int>> GetSaddlePoints()
-    {
-        List<Tuple<int,int>> result = new List<Tuple<int,int>>();
-        
-        int[,] minsEtMaxs = GetZeroArray();
-        
-        GetMaxsOfRowsAndSetToOneInMinsEtMaxsArray(minsEtMaxs);
-        
-        GetMinsOfColumnsAndAddOneInMinsEtMaxsArray(minsEtMaxs);
-        
-        // look for values 2 in the resulting matrix
-        for(int i = 0; i < 5; i++)
+        public int Row { get; private set; }
+
+        public int Column { get; private set; }
+
+        public int Value { get; private set; }
+
+        public Cell(int row, int column, int value)
         {
-            for(int j = 0; j < 5; j++)
-            {
-                if(minsEtMaxs[i,j] == 2) result.Add(new Tuple<int,int>(i,j));
-            }
+            Row = row;
+            Column = column;
+            Value = value;
         }
-        
-        Debug(_array, minsEtMaxs);
-        
-        return result;
+
+        override public bool Equals(object obj)
+        {
+            return Equals(obj as Cell);
+        }
+
+        public bool Equals(Cell other)
+        {
+            if (other == null) return false;
+            return Row == other.Row && Column == other.Column && Value == other.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Row.GetHashCode() ^ Column.GetHashCode() ^ Value.GetHashCode();
+        }
     }
-    
-    private int[,] GetZeroArray()
+
+    public class Matrix
     {
-        return new int[,]
-        { 
+        private int[,] _array;
+
+        public Matrix()
+        {
+            _array = GetZeroArray();
+        }
+
+        public void SetPoint(int row, int column, int value)
+        {
+            _array[row, column] = value;
+        }
+
+        // Saddle point :
+        // value >= any in its row
+        // value <= any in its column
+        public List<Cell> GetSaddlePoints()
+        {
+            List<Cell> maxOfRows = GetCellsWithMaxValueOfEachRow();
+            Debug(_array, maxOfRows, "Maxs of each row :");
+
+            List<Cell> minOfColumns = GetCellsWithMinValueOfEachColumn();
+            Debug(_array, minOfColumns, "Mins of each column :");
+
+            List<Cell> result = maxOfRows.Intersect(minOfColumns).ToList();
+            Debug(_array, result, "Saddle points :");
+
+            return result;
+        }
+
+        private List<Cell> GetCellsWithMaxValueOfEachRow()
+        {
+            List<Cell> result = new List<Cell>();
+            for (int i = 0; i < 5; i++)
+            {
+                List<Cell> row = GetRow(i);
+                IEnumerable<Cell> cellsWithMaxValuesForThisRow = (from cell in row
+                                                                  group cell by cell.Value into grpByValue
+                                                                  orderby grpByValue.Key descending
+                                                                  select grpByValue).First();
+
+                result.AddRange(cellsWithMaxValuesForThisRow);
+            }
+
+            return result;
+        }
+
+        private List<Cell> GetCellsWithMinValueOfEachColumn()
+        {
+            List<Cell> result = new List<Cell>();
+            for (int i = 0; i < 5; i++)
+            {
+                List<Cell> column = GetColumn(i);
+                IEnumerable<Cell> cellsWithMinValuesForThisColumn = (from cell in column
+                                                                     group cell by cell.Value into grpByValue
+                                                                     orderby grpByValue.Key ascending
+                                                                     select grpByValue).First();
+
+                result.AddRange(cellsWithMinValuesForThisColumn);
+            }
+
+            return result;
+        }
+
+        private int[,] GetZeroArray()
+        {
+            return new int[,]
+            {
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0}
-        };
-    }
-    
-    private int[] GetRow(int i)
-    {
-        return new int[] { _array[i,0], _array[i,1], _array[i,2], _array[i,3], _array[i,4] };
-    }
-    
-    private int[] GetColumn(int j)
-    {
-        return new int[] { _array[0,j], _array[1,j], _array[2,j], _array[3,j], _array[4,j] };
-    }
-                                                                                        
-    private int[] GetPositionsOfMaximums(int[] row)
-    {
-        int max = Int32.MinValue;
-        List<int> maximumsPositions = new List<int>();
-        for(int i = 0; i < row.Length; i++)
-        {
-            if(row[i] > max)
-            {
-                max = row[i];
-                maximumsPositions.Clear();
-                maximumsPositions.Add(i);
-            }
-            else if(row[i] == max)
-            {
-                maximumsPositions.Add(i);
-            }
+            };
         }
-        
-        return maximumsPositions.ToArray();
-    }
-    
-    private int[] GetPositionsOfMinimums(int[] column)
-    {
-        int min = Int32.MaxValue;
-        List<int> minimumsPositions = new List<int>();
-        for(int j = 0; j < column.Length; j++)
+
+        private List<Cell> GetRow(int i)
         {
-            if(column[j] < min)
-            {
-                min = column[j];
-                minimumsPositions.Clear();
-                minimumsPositions.Add(j);
-            }
-            else if(column[j] == min)
-            {
-                minimumsPositions.Add(j);
-            }
+            return new List<Cell> {
+                new Cell(i, 0, _array[i, 0]),
+                new Cell(i, 1, _array[i, 1]),
+                new Cell(i, 2, _array[i, 2]),
+                new Cell(i, 3, _array[i, 3]),
+                new Cell(i, 4, _array[i, 4])
+            };
         }
-        
-        return minimumsPositions.ToArray();
-    }
-    
-    private void GetMaxsOfRowsAndSetToOneInMinsEtMaxsArray(int[,] array)
-    {
-        for(int i = 0; i < 5; i++)
+
+        private List<Cell> GetColumn(int j)
         {
-            int[] maxs = GetPositionsOfMaximums(GetRow(i));
-            foreach(int max in maxs)
-            {
-                array[i, max] = 1;
-            }
+            return new List<Cell> {
+                new Cell(0, j, _array[0, j]),
+                new Cell(1, j, _array[1, j]),
+                new Cell(2, j, _array[2, j]),
+                new Cell(3, j, _array[3, j]),
+                new Cell(4, j, _array[4, j])
+            };
         }
-    }
-    
-    private void GetMinsOfColumnsAndAddOneInMinsEtMaxsArray(int[,] array)
-    {
-        for(int j = 0; j < 5; j++)
+
+        private void Debug(int[,] workArray, List<Cell> saddlePoints, string info)
         {
-           int[] mins = GetPositionsOfMinimums(GetColumn(j));
-            foreach(int min in mins)
+            Console.WriteLine(info);
+            // Debug
+            for (int i = 0; i < 5; i++)
             {
-                array[min, j] += 1;
-            }
-        }
-    }
-    
-    private void Debug(int[,] workArray, int[,] minsMaxsArray)
-    {
-        // Debug
-        Console.WriteLine("Work Array :");
-        for(int i = 0; i < 5; i++)
-        {
-            for(int j = 0; j < 5; j++)
-            {
-                Console.Write(workArray[i,j] + " ");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine("MinsMaxs Array :");
-        for(int i = 0; i < 5; i++)
-        {
-            for(int j = 0; j < 5; j++)
-            {
-                Console.Write(minsMaxsArray[i,j] + " ");
+                for (int j = 0; j < 5; j++)
+                {
+                    if (saddlePoints.Contains(new Cell(i, j, _array[i, j])))
+                    {
+                        Console.Write("[" + workArray[i, j] + "]");
+                    }
+                    else
+                    {
+                        Console.Write(" " + workArray[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
             }
             Console.WriteLine();
         }
     }
-    
-    
 }
--- files/SaddlePointsTest.cs
+++ files/SaddlePointsTest.cs
@@ -1,63 +1,66 @@
 using System;
 using NUnit.Framework;
 
-[TestFixture]
-public class SaddlePointsTest
+namespace MySaddlePoints
 {
-    // Saddle point :
-    // value >= any in its row
-    // value <= any in its column
-    
-    [Test]
-    public void Une_matrice_de_zeros_n_a_que_des_saddle_points()
+    [TestFixture]
+    public class SaddlePointsTest
     {
-        SaddlePoints sp = new SaddlePoints();
-        
-        Assert.AreEqual(25, sp.GetSaddlePoints().Count);
+        // Saddle point :
+        // value >= any in its row
+        // value <= any in its column
+
+        [Test]
+        public void Une_matrice_de_zeros_n_a_que_des_saddle_points()
+        {
+            Matrix sp = new Matrix();
+
+            Assert.AreEqual(25, sp.GetSaddlePoints().Count);
+        }
+
+        [Test]
+        public void Une_matrice_avec_une_colonne_incrementielle_a_un_unique_saddle_points()
+        {
+            Matrix sp = new Matrix();
+
+            /*
+                {1, 0, 0, 0, 0},
+                {2, 0, 0, 0, 0},
+                {3, 0, 0, 0, 0},
+                {4, 0, 0, 0, 0},
+                {5, 0, 0, 0, 0}
+            */
+            sp.SetPoint(0, 0, 1);
+            sp.SetPoint(1, 0, 2);
+            sp.SetPoint(2, 0, 3);
+            sp.SetPoint(3, 0, 4);
+            sp.SetPoint(4, 0, 5);
+
+            // a simple example to start you off
+            Assert.AreEqual(1, sp.GetSaddlePoints().Count);
+        }
+
+        [Test]
+        public void Une_matrice_diagonale_a_zero_saddle_points()
+        {
+            Matrix sp = new Matrix();
+
+            /*
+                {1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1}
+            */
+            sp.SetPoint(0, 0, 1);
+            sp.SetPoint(1, 1, 1);
+            sp.SetPoint(2, 2, 1);
+            sp.SetPoint(3, 3, 1);
+            sp.SetPoint(4, 4, 1);
+
+            // a simple example to start you off
+            Assert.AreEqual(0, sp.GetSaddlePoints().Count);
+        }
+
     }
-    
-    [Test]
-    public void Une_matrice_avec_une_colonne_incrementielle_a_un_unique_saddle_points()
-    {
-        SaddlePoints sp = new SaddlePoints();
-        
-        /*
-            {1, 0, 0, 0, 0},
-            {2, 0, 0, 0, 0},
-            {3, 0, 0, 0, 0},
-            {4, 0, 0, 0, 0},
-            {5, 0, 0, 0, 0}
-        */
-        sp.SetPoint(0,0,1);
-        sp.SetPoint(1,0,2);
-        sp.SetPoint(2,0,3);
-        sp.SetPoint(3,0,4);
-        sp.SetPoint(4,0,5);
-        
-        // a simple example to start you off
-        Assert.AreEqual(1, sp.GetSaddlePoints().Count);
-    }
-    
-    [Test]
-    public void Une_matrice_diagonale_a_zero_saddle_points()
-    {
-        SaddlePoints sp = new SaddlePoints();
-        
-        /*
-            {1, 0, 0, 0, 0},
-            {0, 1, 0, 0, 0},
-            {0, 0, 1, 0, 0},
-            {0, 0, 0, 1, 0},
-            {0, 0, 0, 0, 1}
-        */
-        sp.SetPoint(0,0,1);
-        sp.SetPoint(1,1,1);
-        sp.SetPoint(2,2,1);
-        sp.SetPoint(3,3,1);
-        sp.SetPoint(4,4,1);
-        
-        // a simple example to start you off
-        Assert.AreEqual(0, sp.GetSaddlePoints().Count);
-    }
-    
 }
